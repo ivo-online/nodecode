2035032c865b446709563ade27201807092e17d8
ivo-online <ivo.nijhuis@gmail.com>
2022-05-10 13:20:36 +0200

    addes express + ejs

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app
+  .use(express.static('static'))
+  .set('view engine', 'ejs')
+  .set('views', 'view')
+
 app.get('/', (req, res) => {
-  res.send('Hello Worlds!')
+  res.send('<img src="/image/eend.gif" width="150">Hello Worlds!')
+})
+
+app.use((req, res, next) => {
+  console.log('404 error at URL: ' + req.url)
+  res.status(404).render('not_found.ejs', {url: req.url})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Webserver listening on port ${port}`)
 })
